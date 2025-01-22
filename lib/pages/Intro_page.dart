633870986ebb2fd1b5e693e74c52bebf5913bd54
lambt <lambt@vn-sis.com>
2025-01-22 17:05:27 +0700

    first commit

---
+++ lib/pages/Intro_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.shopping_bag,
+                size: 72, color: Theme.of(context).colorScheme.inversePrimary),
+            const SizedBox(
+              height: 25,
+            ),
+            Text(
+              "Minimal shop",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Premium Quality Porduct",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+    colorScheme: ColorScheme.dark(
+        surface: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.white,
+        inversePrimary: Colors.grey.shade700));
